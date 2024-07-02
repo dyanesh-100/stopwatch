447665910d56b0de97ef63dd413de0c9e8b658e5
Dyanesh D <you@example.com>
2024-07-02 21:45:06 +0530

    final commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import StopwatchComponent from './Components/StopwatchComponent/StopwatchComponent'
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <StopwatchComponent/>
+    </React.Fragment>
+  )
+}
+
+export default App
